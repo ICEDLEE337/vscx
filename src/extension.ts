b548ede3e5bea24862257524a27aa1cda566e8a4
Lee Norris <lee.norris@arcadia.io>
2020-08-18 23:19:26 -0400

    chore: worx

--- src/extension.ts
+++ src/extension.ts
@@ -1,69 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { searchHandler, openFolderHandler, cloneHandler, RepoManager, AngularGenerator } from '@onivoro/server-app-vscx';
+import { AngularGenerator, App, IOniExtensionApp } from '@onivoro/server-app-vscx';
 import { join } from 'path';
-// import { join } from 'path';
+
+let app: IOniExtensionApp;
 
 export function activate(context: vscode.ExtensionContext) {
+	app = new App();
 
-	// const assets = GeneratorBase.enumerateAssets(process.cwd(), ['soundcheck', 'dist', 'soundcheck']);
-	// const assets = AngularGenerator.enumerateAssets(context.extensionPath, assetPaths);
-	// vscode.window.showInformationMessage(assets.sort().join(' '));
-
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('we live fam');
-		const assetPaths = ['soundcheck', 'dist', 'soundcheck'];
+	let disposable = vscode.commands.registerCommand(app.registeredCommand, () => {
 
 		const panel = vscode.window.createWebviewPanel(
-			'gitgrok',
-			'GitGrok',
+			app.viewType!,
+			app.title,
 			vscode.ViewColumn.One,
 			{
 				enableScripts: true,
-				localResourceRoots: [vscode.Uri.file(join(context.extensionPath, ...assetPaths))],
+				localResourceRoots: [vscode.Uri.file(join(context.extensionPath, ...app.assetPaths))],
 
 			}
 		);
 
 		panel.webview.onDidReceiveMessage(
-			message => {
-				const postMessage = (command: string, payload: any) => {
-					panel.webview.postMessage({ command, payload });
-				};
-				const { command, payload } = message;
-				switch (command) {
-					case 'search':
-						searchHandler(payload, vscode, postMessage);
-						return;
-					case 'openFile':
-					case 'openFolder':
-						openFolderHandler(payload, vscode);
-						return;
-					case 'clone':
-						cloneHandler(payload, vscode);
-						return;
-					case 'info':
-						vscode.window.showInformationMessage(payload);
-						return;
-					case 'warn':
-						vscode.window.showWarningMessage(payload);
-						return;
-					case 'error':
-						vscode.window.showErrorMessage(payload);
-						return;
-					case 'repoList':
-						new RepoManager().getRepoList().then((repos: any) => {
-							postMessage(command, repos);
-						});
-						return;
-				}
-			},
+			message => app.messageBus(message, vscode),
 			undefined,
 			context.subscriptions
 		);
 		try {
-			const generator = new AngularGenerator(vscode, panel, context, assetPaths);
+			const generator = new AngularGenerator(vscode, panel, context, app.assetPaths);
 			const html = generator.getHtmlWithUris();
 			vscode.window.showInformationMessage(html);
 			panel.webview.html = html;
@@ -77,7 +40,8 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+	app.deactivate();
+}
 
 
